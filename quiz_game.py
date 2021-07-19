5b178d30a14572964c00051edcb13a845a5e357c
RiseT <12624674+RiseT@users.noreply.github.com>
2021-07-20 00:20:20 +0200

    Black all

--- quiz_game.py
+++ quiz_game.py
@@ -10,28 +10,28 @@ score = 0
 
 answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
-    print('Correct!')
+    print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
 answer = input("What does GPU stand for? ")
 if answer.lower() == "graphics processing unit":
-    print('Correct!')
+    print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
 answer = input("What does RAM stand for? ")
 if answer.lower() == "random access memory":
-    print('Correct!')
+    print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
 answer = input("What does PSU stand for? ")
 if answer.lower() == "power supply":
-    print('Correct!')
+    print("Correct!")
     score += 1
 else:
     print("Incorrect!")
